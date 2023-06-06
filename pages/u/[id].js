acdb70708d2f2e1112f6098f894db05480afee24
Ahmed Amawi <ahmed@atgpharma.com>
2023-06-06 14:08:51 -0700

    updated seo

--- pages/u/[id].js
+++ pages/u/[id].js
@@ -2,6 +2,9 @@ import { useForm } from "react-hook-form";
 import { NextSeo } from "next-seo";
 import { createSEOPageConfig } from "../..//utils/seo";
 import { configSet } from "../../lib/pageConfig";
+import Link from "next/link";
+import { PlusIcon, RocketLaunchIcon } from "@heroicons/react/24/outline";
+
 //HOOKS
 import UseCard from "../../hooks/useCard";
 const cards = [
@@ -41,27 +44,38 @@ export default function Home() {
   return (
     <main className="max-w-md mx-auto border">
       <NextSeo {...createSEOPageConfig(configSet.landing)} />
-      <div className="w-full flex flex-col items-center">
-        <div className="h-36 w-full">
-          <img
-            src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-            className="h-36 w-full object-cover opacity-80"
-          />
-        </div>
-        <img
-          src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          className="h-40 w-40 rounded-full -translate-y-24"
-        />
-        <h1 className="font-semibold -mt-20 text-2xl">Ahmed Al Amawi</h1>
-        <p className="text-lg">CTO at Jackrabbit Ops</p>
-        <a
-          href={cards[0].link}
-          className="bg-gray-900 w-2/3 px-8 py-4 rounded-full text-center text-white hover:bg-gray-800 mt-2 text-2xl"
+      <div className="flex flex-row  justify-between items-center px-4 mt-4">
+        <p className="font-bold">Scale YYJ</p>
+        <Link
+          href="/"
+          className="border border-black text-black h-min px-6 py-2 rounded-lg text-sm hover:bg-black hover:text-gray-50"
         >
-          Save Contact
-        </a>
+          <p className="text-sm">See Events</p>
+        </Link>
       </div>
-      <div className="flex flex-row flex-wrap  justify-center gap-8 mt-10 px-6">
+      <div className="w-full flex flex-col items-center">
+        <div className="grid grid-cols-2 mx-4 rounded-xl my-4 bg-black shadow-lg ">
+          <img
+            src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            className="rounded-l-xl "
+          />
+          <div className="flex flex-col justify-center text-gray-50 px-4">
+            <h1 className="font-semibold  text-3xl">Ahmed Al Amawi</h1>
+            <p className="text-sm text-slate-400">Founder @</p>
+            <p className="text-sm  text-slate-400 mt-0 ">Jackrabbit Ops </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1  w-full px-4">
+          <a
+            href={cards[0].link}
+            className="shadow-lg flex flex-row gap-2 items-center justify-center border border-[#F6800A] hover:text-[#F6800A]  px-6 py-2 rounded-lg text-lg w-full text-center bg-[#F6800A] hover:bg-white text-gray-50"
+          >
+            <PlusIcon className="h-5 w-5  text-auto" />
+            Add to Contact
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-row flex-wrap  justify-center gap-8 mt-6 px-6">
         {cards.map((card, index) => (
           <a
             href={card.link}
@@ -73,39 +87,29 @@ export default function Home() {
           </a>
         ))}
       </div>
-      <p className="mt-4 border-t pt-2 px-4 text-lg font-medium text-[#F6800A] ">
-        Magic Search
-      </p>
-      <form
+      <div className="border rounded-lg m-4 my-6 p-2">
+        <p className=" mt-1 text-xl font-semibold text-black  tracking-tighter">
+          Break the Ice with <span className="text-[#F6800A]">AI</span>
+        </p>
+        <p className="text-sm  text-gray-700">
+          We&apos;ll look up Ahmed Al Amawi for you.
+        </p>
+        <div className="w-full flex flex-col items-center justify-center my-3 mt-3">
+          <button
+            type="submit"
+            form="price"
+            className=" w-full flex flex-row justify-center items-center gap-2 text-base bg-black hover:bg-white text-white  hover:text-black px-7 py-2 border border-black  hover:bg-softWhite rounded-lg "
+          >
+            <RocketLaunchIcon className="h-6 w-6" />
+            Launch AI Helper
+          </button>
+        </div>
+      </div>
+      {/* <form
         onSubmit={handleSubmit(magicSearch)}
         id="price"
         className="flex flex-col items-center gap-2 px-4  mb-6"
       >
-        <p
-          className={`text-sm ${
-            errors.search ? "text-red-600" : "text-gray-600"
-          }`}
-        >
-          Input a full name and a city to scrape the internet for details about
-          them.
-        </p>
-        <div
-          className={`${
-            errors.search ? "border-red-600 " : "border-gray-300 "
-          }   rounded-md border p-2 bg-white shadow-sm focus-within:border-indigo-600  w-full  focus-within:ring-1 focus-within:ring-indigo-600 `}
-        >
-          <input
-            type="text"
-            placeholder="Ahmed Al Amawi, Victoria"
-            className={`block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0  focus:outline-none sm:text-lg`}
-            {...register("search", {
-              required: `Please enter a search term`,
-              validate: {
-                minLength: (v) => v.length > 3,
-              },
-            })}
-          />
-        </div>
         <div className="w-max">
           <button
             type="submit"
@@ -113,11 +117,11 @@ export default function Home() {
             className="text-lg bg-[#F6800A] hover:bg-white text-white  hover:text-[#F6800A]  text-center relative px-7 py-3.5 border border-[#F6800A]  hover:bg-softWhite rounded-lg leading-none flex justify-center items-center divide-x divide-gray-600"
           >
             <span className="pb-[0.09rem] transition duration-200 ">
-              Magic Search
+              Get Latest Fun Facts
             </span>
           </button>
         </div>
-      </form>
+      </form> */}
     </main>
   );
 }
