4218de0beed87a20981247a00fd86702f79af202
Ahmed Amawi <ahmed@atgpharma.com>
2023-06-07 11:16:29 -0700

    updated app

--- hooks/useCard.js
+++ hooks/useCard.js
@@ -1,32 +1,71 @@
 import { useState, useEffect } from "react";
-import { useMutation } from "@apollo/client";
+import { useLazyQuery, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
+
+//REQUESTS
+import { USER_LOOKUP } from "../lib/requests";
 
 const UseCard = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [person, setPerson] = useState(null);
+  const [cards, setCards] = useState([]);
+  const [facts, setFacts] = useState(null);
+
   // REQUESTS
   /************************************************************************* */
 
-  // const [inquire, inquiryResponse] = useMutation(SUBMIT_JACKALOPE, {
-  //   fetchPolicy: "network-only",
-  //   onCompleted: (d) => router.push("/thankyou"),
-  //   onError: (d) => alert(d),
-  // });
+  const [loadUser, loadUserRes] = useLazyQuery(USER_LOOKUP, {
+    fetchPolicy: "network-only",
+    onCompleted: (d) => {
+      let person = d.userLookup;
+      setPerson(person);
+      setCards([
+        {
+          src: "/contacts.svg",
+          name: "Contact",
+          link: `data:text/vcard;charset=utf-8,BEGIN:VCARD%0AVERSION:3.0%0AN:${person.lastName};${person.firstName};;;%0ATEL;EMAIL:${person.email}%0AEND:VCARD`,
+        },
+        {
+          src: "/linkedin.svg",
+          name: "LinkedIn",
+          link: person.linkedinUrl,
+        },
+        {
+          src: "/mail.svg",
+          name: "Email",
+          link: `mailto:${person.email}`,
+        },
+      ]);
+    },
+    onError: (d) => alert(d),
+  });
 
   // FUNCTIONS
   /************************************************************************* */
 
+  async function loadPersono(id) {
+    loadUser({
+      variables: {
+        code: id,
+      },
+    });
+  }
+
   async function magicSearch(data) {
     setLoading(true);
 
-    router.push("/magic");
+    // router.push("/magic");
     await new Promise((resolve) => setTimeout(resolve, 2000));
     setLoading(false);
   }
 
   return {
     loading,
+    person,
+    cards,
+    facts,
+    loadPersono,
     magicSearch,
   };
 };
--- pages/u/[id].js
+++ pages/u/[id].js
@@ -1,73 +1,77 @@
-import { useForm } from "react-hook-form";
 import { NextSeo } from "next-seo";
 import { createSEOPageConfig } from "../..//utils/seo";
 import { configSet } from "../../lib/pageConfig";
 import Link from "next/link";
 import { PlusIcon, RocketLaunchIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
 
 //HOOKS
 import UseCard from "../../hooks/useCard";
-const cards = [
-  {
-    src: "/contacts.svg",
-    name: "Contact",
-    link: "data:text/vcard;charset=utf-8,BEGIN:VCARD%0AVERSION:3.0%0AN:Al Amawi;Ahmed;;;%0ATEL;TYPE=CELL:+16476872780%0AEMAIL:ahmed@jackrabbitops.com%0AEND:VCARD",
-  },
-  {
-    src: "/safari.svg",
-    name: "Website",
-    link: "https://www.jackrabbitops.com/",
-  },
-  {
-    src: "/linkedin.svg",
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/ahmed-al-amawi/",
-  },
-  {
-    src: "/mail.svg",
-    name: "Email",
-    link: "mailto:ahmed@jackrabbitops.com",
-  },
-  // {
-  //   src: "/message.svg",
-  //   name: "Number",
-  //   link: "sms:+6476872780",
-  // },
-];
+import { useEffect } from "react";
+
+//COMPONENTS
+import Magic from "../../components/magic";
+
 export default function Home() {
-  const { magicSearch } = UseCard();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const router = useRouter();
+  const { id } = router.query;
+  const { magicSearch, loadPersono, person, cards, loading, facts } = UseCard();
+
+  useEffect(() => {
+    if (!person && id) loadPersono(id);
+  }, [id]);
+
+  if (!person) {
+    return (
+      <main className="max-w-md mx-auto border flex flex-col items-center justify-center h-[100vh]">
+        <svg
+          className="animate-spin h-24 w-24  self-center mr-2 text-[#F6800A]"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 40 40"
+          enableBackground="new 0 0 40 40"
+          xml="preserve"
+        >
+          <path
+            opacity="0.2"
+            d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"
+          />
+          <path d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z" />
+        </svg>
+      </main>
+    );
+  }
   return (
     <main className="max-w-md mx-auto ">
       <NextSeo {...createSEOPageConfig(configSet.landing)} />
       <div className="flex flex-row  justify-between items-center px-4 mt-4">
         <p className="font-bold">Scale YYJ</p>
         <Link
-          href="/"
+          href="https://www.jackrabbitops.com/"
           className="border border-black text-black h-min px-6 py-2 rounded-lg text-sm hover:bg-black hover:text-gray-50"
         >
           <p className="text-sm">See Events</p>
         </Link>
       </div>
       <div className="w-full flex flex-col items-center">
-        <div className="grid grid-cols-2 mx-4 rounded-xl my-4 bg-black shadow-lg ">
+        <div className="h-52 grid grid-cols-2 mx-4 rounded-xl my-4 bg-black shadow-lg ">
           <img
-            src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            className="rounded-l-xl "
+            src={person.linkedinProfilePicUrl}
+            className="rounded-l-xl object-cover h-full w-full"
           />
-          <div className="flex flex-col justify-center text-gray-50 p-4">
-            <h1 className="font-semibold  text-3xl">Ahmed Al Amawi</h1>
-            <p className="text-sm text-slate-400">Founder @</p>
-            <p className="text-sm  text-slate-400 mt-0 ">Jackrabbit Ops </p>
+          <div className="flex flex-col justify-center text-gray-50 p-4 ">
+            <h1 className="font-semibold  text-3xl">
+              {person.firstName} {person.lastName}
+            </h1>
+            <p className="text-sm text-slate-400">{person.position} @</p>
+            <p className="text-sm  text-slate-400 mt-0 ">
+              {person.companyName}
+            </p>
           </div>
         </div>
         <div className="grid grid-cols-1  w-full px-4">
           <a
-            href={cards[0].link}
+            href={cards[0]?.link}
             className="shadow-lg flex flex-row gap-2 items-center justify-center border border-[#F6800A] hover:text-[#F6800A]  px-6 py-2 rounded-lg text-lg w-full text-center bg-[#F6800A] hover:bg-white text-gray-50"
           >
             <PlusIcon className="h-5 w-5  text-auto" />
@@ -87,41 +91,45 @@ export default function Home() {
           </a>
         ))}
       </div>
-      <div className="border rounded-lg m-4 my-6 p-2">
-        <p className=" mt-1 text-xl font-semibold text-black  tracking-tighter">
-          Break the Ice with <span className="text-[#F6800A]">AI</span>
-        </p>
-        <p className="text-sm  text-gray-700">
-          We&apos;ll look up Ahmed Al Amawi for you.
-        </p>
-        <div className="w-full flex flex-col items-center justify-center my-3 mt-3">
-          <button
-            type="submit"
-            form="price"
-            className=" w-full flex flex-row justify-center items-center gap-2 text-base bg-black hover:bg-white text-white  hover:text-black px-7 py-2 border border-black  hover:bg-softWhite rounded-lg "
-          >
-            <RocketLaunchIcon className="h-6 w-6" />
-            Launch AI Helper
-          </button>
+      {!facts ? (
+        <div className="border rounded-lg m-4 my-6 p-2">
+          <p className=" mt-1 text-xl font-semibold text-black  tracking-tighter">
+            Break the Ice with <span className="text-[#F6800A]">AI</span>
+          </p>
+          <p className="text-sm  text-gray-700">
+            We&apos;ll look up {person.firstName} {person.lastName} for you.
+          </p>
+          <div className="w-full flex flex-col items-center justify-center my-3 mt-3">
+            <button
+              type="submit"
+              onClick={() => magicSearch()}
+              form="price"
+              className=" w-full flex flex-row justify-center items-center gap-2 text-base bg-black hover:bg-white text-white  hover:text-black px-7 py-2 border border-black  hover:bg-softWhite rounded-lg "
+            >
+              <RocketLaunchIcon className="h-6 w-6" />
+              Launch AI Helper
+              {loading && (
+                <svg
+                  className="animate-spin h-7 w-7  ml-2 self-center "
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 40 40"
+                  enableBackground="new 0 0 40 40"
+                  xml="preserve"
+                >
+                  <path
+                    opacity="0.2"
+                    d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"
+                  />
+                  <path d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z" />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
-      </div>
-      {/* <form
-        onSubmit={handleSubmit(magicSearch)}
-        id="price"
-        className="flex flex-col items-center gap-2 px-4  mb-6"
-      >
-        <div className="w-max">
-          <button
-            type="submit"
-            form="price"
-            className="text-lg bg-[#F6800A] hover:bg-white text-white  hover:text-[#F6800A]  text-center relative px-7 py-3.5 border border-[#F6800A]  hover:bg-softWhite rounded-lg leading-none flex justify-center items-center divide-x divide-gray-600"
-          >
-            <span className="pb-[0.09rem] transition duration-200 ">
-              Get Latest Fun Facts
-            </span>
-          </button>
-        </div>
-      </form> */}
+      ) : (
+        <Magic />
+      )}
     </main>
   );
 }
